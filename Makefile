4e027987673bfbc639ff1d265aaf347b558c7748
Daniel Schaefer <daniel.schaefer@hpe.com>
2020-11-26 23:54:21 +0800

    Add scaffolding

---
+++ Makefile
@@ -0,0 +1,10 @@
+.PHONY: clean
+
+presentation.pdf: presentation.tex $(wildcard resources/*)
+	latexmk -pdflatex="pplatex -c pdflatex --" -pdf -interaction=nonstopmode presentation.tex 2>&1 | tee latexmk_log.txt
+
+clean:
+	latexmk -C
+	rm -f presentation.bbl presentation.run.xml \
+		presentation.nav presentation.snm presentation.vrb latexmk_log.txt \
+		result
---
+++ default.nix
@@ -0,0 +1,71 @@
+with import <nixpkgs> {};
+
+let
+  latexPackage = texlive.combine {
+    inherit (texlive)
+      scheme-small
+
+      latexmk
+
+      beamer
+      beamertheme-metropolis
+      appendixnumberbeamer
+      ccicons
+      pgfopts
+      pgfplots
+
+      biber
+      biblatex
+      biblatex-apa
+      csquotes
+      logreq
+      xstring
+
+      siunitx  
+      ;
+  };
+
+  pplatex =
+    stdenv.mkDerivation rec {
+      name = "pplatex-${version}";
+      version = "1.0-rc2";
+
+      src = fetchFromGitHub {
+        owner = "stefanhepp";
+        repo = "pplatex";
+        rev = name;
+        sha256 = "0xw7nvi2l15iyp9sm8vmmqghi54v99bcivqvx89f5v2gw0kw47k3";
+      };
+
+      buildInputs = [ cmake pkgconfig pcre ];
+
+      installPhase = ''
+        install -D src/pplatex $out/bin/pplatex
+      '';
+    };
+in
+  stdenv.mkDerivation rec {
+    name = "presentation-template";
+
+    src = ./.;
+
+    buildInputs = [
+      latexPackage pplatex pdfpc
+    ];
+
+    # We could be building from an unclean directory, so remove intermediate files first
+    preBuild = "make clean";
+
+    installPhase = ''
+      install -D presentation.pdf $out
+    '';
+
+    checkPhase = ''
+      function build_error() {
+        echo "=====> There have been warnings or errors in the last run, fix them!"
+        exit 1
+      }
+
+      cat latexmk_log.txt | grep "o) Errors:" | tail -1 | grep "o) Errors: 0, Warnings: 0" || build_error
+    '';
+  }
